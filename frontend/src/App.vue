e25aada7ab19f545e05fc34c8d2a0c69408ba039
rahayon <rahidul_islam@criptext.com>
2021-04-30 14:24:58 +0600

    initialize of frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <HelloWorld msg="Thank you for using our product. We're glad you're with us."/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~@/../mdb/scss/index.free.scss';
+
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+</style>
